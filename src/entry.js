3ff44f3df03bf8c8ff74721848943cd8e0d63fe9
Edward <e.arechwa@gmail.com>
2025-01-09 20:52:08 +0100

    refactor: new start

--- src/entry.js
+++ src/entry.js
@@ -1,14 +1,2 @@
-import { createStartScreen } from './scripts/screens/start';
+import './scripts';
 import './styles/index.css';
-
-const appContainer = document.getElementById('app');
-
-if (!appContainer) throw new Error('Container not provided');
-
-// TODO: add separate module for game logic
-function startGame(level) {
-  console.log(`Game started with level: ${level}`);
-}
-
-const startScreen = createStartScreen(startGame);
-appContainer.appendChild(startScreen);
